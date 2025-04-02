4f6329cfa99e7ae576fe77c82e6f9a3e085f3938
slysiele <sieleisilvestor@gmail.com>
2025-04-03 00:01:20 +0300

    changed config file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://sylvestorsielei:Sly@2000@clustera.p8j6llj.mongodb.net/?retryWrites=true&w=majority&appName=ClusterA',
+    development: 'mongodb+srv://sylvestorsielei:Sly@2000@clustera.p8j6llj.mongodb.net/?retryWrites=true&w=majority&appName=ClusterA',
+    test: 'mongodb+srv://sylvestorsielei:Sly@2000@clustera.p8j6llj.mongodb.net/?retryWrites=true&w=majority&appName=ClusterA',
 }
 module.exports = config;
